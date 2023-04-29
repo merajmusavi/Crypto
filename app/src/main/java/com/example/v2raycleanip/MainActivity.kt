0db7f97e4227e4fca93ea82ebd5b66702deb2b74
merajuni7ed <merajmusavi1243m@gmail.com>
2023-04-29 17:20:27 +0330

    new Update

--- app/src/main/java/com/example/v2raycleanip/MainActivity.kt
+++ app/src/main/java/com/example/v2raycleanip/MainActivity.kt
@@ -2,13 +2,31 @@ package com.example.v2raycleanip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recyclerviewkotlin.MyAdapter
+import com.example.v2raycleanip.databinding.ActivityMainBinding
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val client = OkHttpClient()
+        val request = Request.Builder().url("https://api.wallex.ir/v1/currencies/stats").build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                TODO("Not yet implemented")
+            }
 
+            override fun onResponse(call: Call, response: Response) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
